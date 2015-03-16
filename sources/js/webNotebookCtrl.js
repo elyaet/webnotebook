55b860e0b7540e75fb7f96a018cf039b959c7251
elyaet <elyaet@gmail.com>
2015-03-16 23:54:11 +0100

    Include AngularJs. Initialize Yunohost conf

---
+++ sources/js/webNotebookCtrl.js
@@ -0,0 +1,9 @@
+/**
+ * Created by elyaet on 16/03/15.
+ */
+app.controller("webNotebookCtrl", function($scope) {
+    $scope.message = "";
+    $scope.left  = function() {return 100 - $scope.message.length;};
+    $scope.clear = function() {$scope.message = "";};
+    $scope.save  = function() {alert("Note Saved");};
+});
